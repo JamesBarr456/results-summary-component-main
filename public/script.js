5e12951e47c957cbc3f5e97f8a603712f5916fa8
Emmanuel Barreto <barretosantiago@hotmail.com>
2023-05-07 00:41:38 -0300

    se agregan los valores por js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,44 @@
 import data from "./data.json" assert {type: "json"};
 
-let icon = document.querySelectorAll(".icon");
+
+//Funcion 
 
 function createNode(element) {
     return document.createElement(element);
 }
 
-function append(parent, el) {
-  return parent.appendChild(el);
-}
-data.forEach(element => {
-    console.log(data);
-    icon[0].setAttribute("src", element[0])
-})
+//----------------------------------------------------------------
+
+//variables 
+
+let suma_total = 0;
+let cantidad = 0;
+let icon = document.getElementsByClassName("icon");
+let title = document.getElementsByClassName("ability__icon");
+let point = document.getElementsByClassName("point");
+let total = document.getElementsByClassName("score__big");
+
+//------------------------------------------------------------------
+
+data.forEach((element, index) => {
+    //agregamos el icono
+    icon[index].setAttribute("src", element["icon"]);
+
+    //agregar la etiqueta span
+    let span = createNode("span");
+    span.innerHTML = element["category"];
+    title[index].append(span);
+
+    //agregar score
+    point[index].innerHTML = element["score"];
+
+    suma_total += element["score"];
+    cantidad += 1;
+});
+
+//-------------------------------------------------------------------
+
+//Agregar valor promedio
+let promedio = parseInt(suma_total/cantidad);
+total[0].innerHTML = promedio;
+
